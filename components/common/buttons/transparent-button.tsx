deb9cf683a6c77232d0078b0fb2ca8c4ac134904
Gonzalo Gil <gonzalo.gil@caramelpoint.com>
2022-12-01 13:10:46 -0300

    feat(dashboard layout): created dashboard layout

---
+++ components/common/buttons/transparent-button.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const TransparentButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  font-size: 20px;
+  padding: 0;
+  :hover {
+    cursor: pointer;
+  }
+  :focus {
+    outline: none;
+  }
+  height: fit-content;
+  width: 100%;
+  color: ${({ theme }) => theme.palette.colors.smalt};
+`
