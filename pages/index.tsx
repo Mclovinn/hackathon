acfecfb6c3645c1529e6cefed983422f4cecfa09
Leandro Falugi <leandro.falugi@caramelpoint.com>
2022-12-01 16:24:27 -0300

    feat: add orders table component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Head from 'next/head'
 import { ReactElement } from 'react'
 import DummyMain from '../components/DummyMain'
+import OrdersTable from '../components/OrdersTable'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 const Landing = (): ReactElement => {
   return (
     <div>
-      <Head>
-        <title>My App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <ThemeProvider theme={darkTheme}>
+        <Head>
+          <title>My App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <DummyMain />
-      </main>
+        <main>
+          <DummyMain />
+
+          <OrdersTable />
+        </main>
+      </ThemeProvider>
     </div>
   )
 }
