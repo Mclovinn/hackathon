6057ba21f979a353259ed0b5c37ae59b9a9b5ee2
andreswittouck <andreswittouck@gmail.com>
2022-12-02 12:40:03 -0300

    fix(status): coderStatus

--- types/order-schema.tsx
+++ types/order-schema.tsx
@@ -4,11 +4,11 @@ import { OrderStatus } from './order-status'
 const OrderSchema = Joi.object({
   sku: Joi.string().uuid(),
   status: Joi.string(),
-  destinationAddress: Joi.string().valid(OrderStatus.Delivered, OrderStatus.InTransit),
+  destinationAddress: Joi.string().valid(OrderStatus.DELIVERED, OrderStatus.IN_TRANSIT),
   trackingId: Joi.string().uuid(),
   manifestId: Joi.string().uuid(),
   created: Joi.date().timestamp(),
-  delivered: Joi.date().timestamp(),
+  DELIVERED: Joi.date().timestamp(),
   shipped: Joi.date().timestamp(),
 })
 export default OrderSchema
