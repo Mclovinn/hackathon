5effcf32828d4fc88ef1b303578a017c0a7adea9
Bruno Bertoni <bruno@caramelpoint.com>
2022-12-02 10:31:33 -0300

    add geopoint.

--- models/order.tsx
+++ models/order.tsx
@@ -1,11 +1,13 @@
 import { model } from 'dynamoose'
 import { Item } from 'dynamoose/dist/Item'
+import { Geopoint } from './geopoint.type'
 
 export class OrderItem extends Item {
   id: string | undefined
   status: string | undefined
   sku: string | undefined
-  destinationAddress: string | undefined
+  destinationAddress: Geopoint | undefined
+  sourceAddress: Geopoint | undefined
   trackingId: string | undefined
   manifestId: string | undefined
   created: string | undefined
@@ -27,7 +29,18 @@ export const OrderModel = model<OrderItem>(
       type: String,
     },
     destinationAddress: {
-      type: String,
+      type: Object,
+      schema: {
+        latitude: String,
+        longitude: String,
+      },
+    },
+    sourceAddress: {
+      type: Object,
+      schema: {
+        latitude: String,
+        longitude: String,
+      },
     },
     trackingId: {
       type: String,
