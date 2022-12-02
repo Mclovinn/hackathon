6dbcd05222c48d6f8c23ca886badd83c60ead044
Leandro Falugi <leandro.falugi@caramelpoint.com>
2022-12-02 09:56:40 -0300

    fix: fix build 3

--- components/sections/orders.tsx
+++ components/sections/orders.tsx
@@ -3,7 +3,7 @@ import { DataGrid, GridRowsProp, GridColDef, GridToolbar } from '@mui/x-data-gri
 import type {} from '@mui/x-data-grid/themeAugmentation'
 import { Box } from '@mui/material'
 import styled from 'styled-components'
-import { Dropdown } from '../common/dropdown.component'
+import { Dropdown } from '../common/dropdown'
 
 const $GridContainer = styled.div`
   background-color: ${({ theme }) => theme.palette.colors.nero};
