c72dc1a58351bc5e8c7d31c2aae32db31f7af873
andreswittouck <andreswittouck@gmail.com>
2022-12-02 15:51:24 -0300

    fix(patchOrder): add body status}

--- pages/api/orders/[id].ts
+++ pages/api/orders/[id].ts
@@ -1,15 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import OrderService from '../../../services/OrderService'
 import { deliverOrder } from '../../../services/logistic.service'
+import { OrderStatus } from '../../../types/order-status'
 
 const handler = async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'PATCH') {
     try {
-      let order = await OrderService.changeOrderToDelivered({ req, res })
-      if (order) {
-        const origin = order.destinationAddress
-        const txHash = order.trackingId && (await deliverOrder(order.trackingId, JSON.stringify(origin)))
-        return res.status(200).json({ order, txHash })
+      if (req.body.status === OrderStatus.DELIVERED) {
+        let order = await OrderService.changeOrderToDelivered({ req, res })
+        if (order) {
+          const origin = order.destinationAddress
+          const txHash = order.trackingId && (await deliverOrder(order.trackingId, JSON.stringify(origin)))
+          return res.status(200).json({ order, txHash })
+        }
       }
     } catch (e) {
       console.log(e)
