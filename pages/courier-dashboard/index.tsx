f482a1a1f7a4f562775c18af511c31f0092bd95d
agustencp <agustin.munoz@caramelpoint.com>
2023-03-15 13:01:02 -0300

    feat: cards test

--- pages/courier-dashboard/index.tsx
+++ pages/courier-dashboard/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head'
-import { ReactElement, useEffect, useState } from 'react'
-import { Button, CircularProgress, Typography } from '@mui/material'
+import { cloneElement, ReactElement, useEffect, useState } from 'react'
+import {
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material'
 import styled from 'styled-components'
 import { useStoreState } from '../../store/hooks'
 import { PrivatePage } from '../../components/routing/private-page'
 import router from 'next/router'
 import QrReaderModal from './qr-reader-modal'
 import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner'
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined'
+import CheckIcon from '@mui/icons-material/Check'
 
 const $Container = styled.div`
   display: flex;
@@ -16,6 +28,7 @@ const $Container = styled.div`
   flex-direction: column;
   padding: 60px 0;
   gap: 40px;
+  width: 100%;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktopS}) {
     justify-content: center;
@@ -26,14 +39,22 @@ const $Container = styled.div`
 const $Title = styled(Typography)`
   font-size: 1.3rem;
   align-self: flex-start;
-  margin-left: 30px;
+  padding-left: 40px;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktopS}) {
     align-self: center;
     justify-content: center;
     max-width: 800px;
     height: auto;
-    margin-left: 0;
+    padding-left: 0;
+  }
+`
+
+const $CardTitle = styled(Typography)`
+  display: flex;
+  align-items: center;
+  svg {
+    width: 50px;
   }
 `
 
@@ -47,6 +68,14 @@ const CourierDashboard = (): ReactElement => {
   useEffect(() => {
     if (sessionModel.session.name) setUsername(sessionModel.session.name)
   }, [sessionModel.session.name])
+
+  function generate(element: ReactElement) {
+    return [0, 1, 2].map(value =>
+      cloneElement(element, {
+        key: value,
+      })
+    )
+  }
 
   useEffect(() => {
     if (qrCode) {
@@ -64,6 +93,58 @@ const CourierDashboard = (): ReactElement => {
         </Head>
         <$Container>
           <$Title variant="h5">Welcome{username && `, ${username}`}!</$Title>
+
+          <Card
+            sx={{
+              width: {
+                xs: 0.8,
+                md: 600,
+              },
+            }}
+            variant="outlined"
+          >
+            <CardContent>
+              <$CardTitle gutterBottom variant="h6">
+                <DescriptionOutlinedIcon />
+                Active Orders
+              </$CardTitle>
+              <List dense>
+                {generate(
+                  <ListItem>
+                    <ListItemIcon>
+                      <DescriptionOutlinedIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Single-line item" />
+                  </ListItem>
+                )}
+              </List>
+            </CardContent>
+          </Card>
+          <Card
+            sx={{
+              width: {
+                xs: 0.8,
+                md: 600,
+              },
+            }}
+          >
+            <CardContent>
+              <$CardTitle gutterBottom variant="h6">
+                <CheckIcon />
+                Orders Delivered
+              </$CardTitle>
+              <List dense>
+                {generate(
+                  <ListItem>
+                    <ListItemIcon>
+                      <DescriptionOutlinedIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Single-line item" />
+                  </ListItem>
+                )}
+              </List>
+            </CardContent>
+          </Card>
 
           {!qrCode ? (
             <Button variant="contained" onClick={() => setShowScanner(true)} startIcon={<QrCodeScannerIcon />}>
