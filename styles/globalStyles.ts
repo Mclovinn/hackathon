b3bc2df07775a69b33e2ffbe2262de21109e55f0
andreswittouck <andres@caramelpoint.com>
2023-03-30 18:03:18 -0300

    fix(ordersPage): change the table styles

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -3,6 +3,7 @@ import '@fontsource/roboto/700.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/300.css'
+import '@fontsource/lato/400.css'
 
 const GlobalStyle = createGlobalStyle`
     html,
