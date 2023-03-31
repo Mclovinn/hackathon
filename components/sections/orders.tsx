90c10ce6a4f361a1340a5e1ec6bdfab5c4bdfe02
Chompy Wittouck <60898121+andreswittouck@users.noreply.github.com>
2023-03-31 12:57:18 -0300

    fix(ordersPage): change the table styles (#54)

--- components/sections/orders.tsx
+++ components/sections/orders.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
 import { DataGrid, GridColDef, GridToolbar, GridSelectionModel, GridRenderCellParams } from '@mui/x-data-grid'
-import { Box, IconButton } from '@mui/material'
+import { Box, BoxProps, Button, ButtonProps, IconButton } from '@mui/material'
 import styled from 'styled-components'
 import { Dropdown } from '../common/dropdown.component'
 import { useQuery } from 'react-query'
@@ -20,7 +20,7 @@ import { QRCodeCanvas } from 'qrcode.react'
 const $GridContainer = styled.div`
   background-color: ${({ theme }) => theme.palette.colors.nero};
   height: fit-content;
-  width: 1200px;
+  width: 100%;
   height: auto;
   margin: 20px auto;
   border-radius: 12px;
@@ -31,11 +31,10 @@ const $GridHeader = styled.div`
   justify-content: right;
   align-items: center;
   padding: 15px;
-  margin-top: 40px;
+  margin-top: 30px;
 `
 
 const $LoaderWrapper = styled.div`
-  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -44,6 +43,74 @@ const $LoaderWrapper = styled.div`
 const $SectionCode = styled.div`
   display: none;
 `
+
+const $StyledBox = styled((props: BoxProps) => <Box {...props} />)(({ theme }) => ({
+  height: 669,
+  width: '100%',
+  padding: '0 28px 0px 35px',
+  marginTop: '-57px',
+
+  '& .MuiDataGrid-root': {
+    '& .MuiDataGrid-toolbarContainer button': {
+      color: theme.palette.colors.summerSky,
+      margin: '9px',
+    },
+
+    '& .MuiDataGrid-columnHeadersInner': {
+      '& .MuiDataGrid-columnHeaderTitle': {
+        fontWeight: 400,
+      },
+      '& .css-1e6ud36-MuiButtonBase-root-MuiCheckbox-root.Mui-checked': {
+        color: theme.palette.colors.summerSky,
+      },
+      '& .MuiDataGrid-iconSeparator': {
+        display: 'none',
+      },
+    },
+
+    '& .MuiDataGrid-row': {
+      fontFamily: 'Lato',
+      fontSize: '0.813rem',
+      '&.Mui-selected': {
+        background: 'transparent',
+        '& div span': {
+          color: theme.palette.colors.summerSky,
+        },
+      },
+    },
+
+    '& .MuiDataGrid-footerContainer': {
+      padding: '13px 0',
+      borderBlockStart: 'none',
+    },
+  },
+  '& .super-app-theme--header': {
+    fontSize: '1rem',
+  },
+}))
+
+const $StyledButton = styled((props: ButtonProps) => <Button {...props} />)(({ theme }) => ({
+  width: '128px',
+  fontFamily: 'Lato',
+  padding: '2px 16px',
+  fontWeight: 400,
+  fontSize: '0.813rem',
+
+  '&.READY_TO_FULFILL': {
+    background: theme.palette.colors.warningMain,
+    color: theme.palette.colors.white,
+  },
+
+  '&.DELIVERED': {
+    background: theme.palette.colors.greenLight,
+    color: theme.palette.colors.white,
+  },
+
+  '&.IN_TRANSIT': {
+    borderColor: theme.palette.colors.greenLight,
+    color: theme.palette.colors.white,
+  },
+}))
 
 export type TableRowType = {
   id: string
@@ -98,17 +165,37 @@ export function Orders() {
     },
   })
 
+  const ButtonType = (status: OrderStatus) => {
+    return (
+      <$StyledButton
+        variant={status === OrderStatus.IN_TRANSIT ? 'outlined' : 'contained'}
+        className={status}
+        disableElevation
+      >
+        {status.replace(/_/g, ' ')}
+      </$StyledButton>
+    )
+  }
+
   const columns: GridColDef[] = [
     { headerClassName: 'super-app-theme--header', field: 'id', headerName: 'Order ID', width: 300 },
     { headerClassName: 'super-app-theme--header', field: 'sku', headerName: 'SKU', width: 100 },
-    { headerClassName: 'super-app-theme--header', field: 'status', headerName: 'Status', width: 120 },
-    { headerClassName: 'super-app-theme--header', field: 'address', headerName: 'Destination Address', width: 150 },
+    {
+      headerClassName: 'super-app-theme--header',
+      field: 'status',
+      headerName: 'Status',
+      width: 148,
+      align: 'center',
+      renderCell: (params: GridRenderCellParams<any, TableRowType>) => ButtonType(params.row.status),
+    },
+    { headerClassName: 'super-app-theme--header', field: 'address', headerName: 'Destination Address', width: 185 },
     { headerClassName: 'super-app-theme--header', field: 'tracking', headerName: 'Tracking ID', width: 300 },
+    { headerClassName: 'super-app-theme--header', field: 'manifest', headerName: 'Manifest', width: 150 },
     {
       headerClassName: 'super-app-theme--header',
       field: 'document',
       headerName: 'Shipping Document',
-      width: 150,
+      width: 180,
       align: 'center',
       renderCell: (params: GridRenderCellParams<any, TableRowType>) =>
         params.row.status !== OrderStatus.READY_TO_FULFILL && (
@@ -174,15 +261,7 @@ export function Orders() {
         <$GridHeader>
           <Dropdown onDeliveredSubmit={setOrderAsDeliveredAction} onInitializeSubmit={onInitializeSubmit} />
         </$GridHeader>
-        <Box
-          sx={{
-            height: 600,
-            padding: '0 20px',
-            '& .super-app-theme--header': {
-              fontSize: '20px',
-            },
-          }}
-        >
+        <$StyledBox>
           <DataGrid
             disableDensitySelector
             disableColumnSelector
@@ -203,7 +282,7 @@ export function Orders() {
               setSelectedRows(selected)
             }}
           />
-        </Box>
+        </$StyledBox>
         {initializeOrdersMutation.isLoading && (
           <$LoaderWrapper>
             <BounceLoader color="#7A27E8" />
